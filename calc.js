45c46db6f2a3c3c9ab5529a3264e741d3c3916b2
Christopher Peters <53628686+stardust1689@users.noreply.github.com>
2021-03-30 10:37:28 -0800

    Add files via upload

--- calc.js
+++ calc.js
@@ -1,70 +1,15 @@
-// calculate(exp) {
-//     for item in extension {
-//         if item == "**" {
-//             replace item and two adacent items (num1 and num2) with num1 ** num2 
-//         }
-//     }
-//     for item in extension {
-//         if item == "*" {
-//             replace item and two adacent items (num1 and num2) with num1 * num2 
-//         }
-//         if item == "/" {
-//             replace item and two adacent items (num1 and num2) with num1 / num2 
-//         }
-//     }
-//     for item in extension {
-//         if item == "+" {
-//             replace item and two adacent items (num1 and num2) with num1 + num2 
-//         }
-//         if item == "-*" {
-//             replace item and two adacent items (num1 and num2) with num1 - num2 
-//         }
-//     }
-//     return last item in exp (will be a number)
-// }
+let expressions = [];
 
-function calculate(expression) {
-    // let exp = expression.slice();
-    // console.log(exp);
-    // for (item of exp) {
-    //     let index = exp.indexOf(item);
-    //     if (item === "**") {
-    //         let exponent = exp[index - 1]**exp[index + 1];
-    //         exp.splice(index-1, 3, exponent);
-    //         console.log(exp);
-    //     }
-    // }
-    // for (item of exp) {
-    //     let index = exp.indexOf(item);
-    //     if (item === "*") {
-    //         let product = exp[index - 1] * exp[index + 1];
-    //         exp.splice(index-1, 3, product);
-    //         console.log(exp);
-    //     } else if (item === "/") {
-    //         let quotient = exp[index - 1] / exp[index + 1];
-    //         exp.splice(index-1, 3, quotient);
-    //         console.log(exp);
-    //     }
-    // }
-    // for (item of exp) {
-    //     let index = exp.indexOf(item);
-    //     if (item === "+") {
-    //         let sum = exp[index - 1] + exp[index + 1];
-    //         exp.splice(index-1, 3, sum);
-    //         console.log(exp);
-    //     } else if (item === "-") {
-    //         let difference = exp[index - 1] - exp[index + 1];
-    //         exp.splice(index-1, 3, difference);
-    //         console.log(exp);
-    //     }
-    // }
+const calculate = expression => {
     let exp = expression.slice();
-    while (exp.length > 1) {
+    while (exp.length > 1) {        
+
         while (exp.includes("**")) {
             let index = exp.indexOf("**");
             let add = exp[index - 1]**exp[index + 1];
             exp.splice(index-1, 3, add);
         }
+
         while (exp.includes("*") || exp.includes("/")) {
             let index = 0;
             if (exp.includes("*") && exp.includes("/")) { index = Math.min(exp.indexOf("*"), exp.indexOf("/")); }
@@ -73,10 +18,11 @@ function calculate(expression) {
 
             let add = 0;
             if (exp[index] === "*") { add = exp[index - 1] * exp[index + 1] }
-            else { add = exp[index - 1] / exp[index + 1] }
+            else { add = exp[index - 1] / exp[index + 1]; }
 
             exp.splice(index-1, 3, add);
         }
+
         while (exp.includes("+") || exp.includes("-")) {
             let index = 0;
             if (exp.includes("+") && exp.includes("-")) { index = Math.min(exp.indexOf("+"), exp.indexOf("-")); }
@@ -84,14 +30,25 @@ function calculate(expression) {
             else { index = exp.indexOf("-"); }
 
             let add = 0;
-            if (exp[index] === "+") { add = exp[index - 1] + exp[index + 1] }
+            if (exp[index] === "+") { add = exp[index - 1] + exp[index + 1]; }
             else { add = exp[index - 1] - exp[index + 1] }
 
             exp.splice(index-1, 3, add);
         }
     }
+
     return exp[0]
 }
+
+const calcSin = exp => Math.sin(calculate(exp));
+
+const calcCos = exp => Math.cos(calculate(exp));
+
+const calcTan = exp => Math.tan(calculate(exp));
+
+const calcLog10 = exp => Math.log10(calculate(exp));
+
+const calcLn = exp => Math.log(calculate(exp));
 
 let exp2 = [1, "-", 3, "*", 2, "/", 3, "-", 1, "+", 2, "*", 2, "-", 2, "+",1, "*",3, "*", 3, "+", 1, "-", 1, "**", 2]
 // let exp2 = [1, "-", 3]
