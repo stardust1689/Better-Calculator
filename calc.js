9877bb0441f05437eebe5ba8214cf8d85c5fb274
Christopher Peters <53628686+stardust1689@users.noreply.github.com>
2021-04-02 00:55:31 -0800

    Add files via upload

--- calc.js
+++ calc.js
@@ -11,6 +11,7 @@ const validKeys = [
     "8",
     "9",
     "0",
+    ".",
     "+",
     "-",
     "*",
@@ -30,13 +31,13 @@ let numbers = ("1234567890.")
 const specialKeysCheck = event => {
     if (event.key === "s") {
         event.preventDefault();
-        document.getElementById("exp-input").value += "sin(";  
+        document.getElementById("exp-input").value += "sin";  
     } else if (event.key === "c") {
         event.preventDefault();
-        document.getElementById("exp-input").value += "cos(";  
+        document.getElementById("exp-input").value += "cos";  
     } else if (event.key === "t") {
         event.preventDefault();
-        document.getElementById("exp-input").value += "tan(";
+        document.getElementById("exp-input").value += "tan";
     } else if (event.key === "p") {
         event.preventDefault();
         document.getElementById("exp-input").value += "pi";       
@@ -45,26 +46,33 @@ const specialKeysCheck = event => {
         document.getElementById("exp-input").value += "e";    
     } else if (event.key === "l") {
         event.preventDefault();
-        document.getElementById("exp-input").value += "log(";   
+        document.getElementById("exp-input").value += "log";   
     } else if (event.key === "n") {
         event.preventDefault();
-        document.getElementById("exp-input").value += "ln(";
+        document.getElementById("exp-input").value += "ln";
     } else if (event.key === "Enter") {
         let newExpression = document.getElementById("exp-input").value
-        newExpression = newExpression.replace("sin", "s");
-        newExpression = newExpression.replace("cos", "c");
-        newExpression = newExpression.replace("tan", "t");
-        newExpression = newExpression.replace("pi", "p");
-        newExpression = newExpression.replace("log", "l");
-        newExpression = newExpression.replace("ln", "n");
-        newExpression = newExpression.match(/[sctpeln()\+\-*\/^]|[0-9]+/g);
-        // for (let i = 0; i < newExpression.length; i++) {
-
-        // }
-        // newExpression = newExpression.split("");
-        
         console.log(newExpression);
-        
+        newExpression = newExpression.replaceAll("asin", "S");
+        newExpression = newExpression.replaceAll("acos", "C");
+        newExpression = newExpression.replaceAll("atan", "T");
+        newExpression = newExpression.replaceAll("sin^-1", "S");
+        newExpression = newExpression.replaceAll("cos^-1", "C");
+        newExpression = newExpression.replaceAll("tan^-1", "T");        
+        newExpression = newExpression.replaceAll("sin", "s");
+        newExpression = newExpression.replaceAll("cos", "c");
+        newExpression = newExpression.replaceAll("tan", "t");
+        newExpression = newExpression.replaceAll("pi", "p");
+        newExpression = newExpression.replaceAll("log", "l");
+        newExpression = newExpression.replaceAll("ln", "n");
+        console.log(newExpression);
+        newExpression = newExpression.match(/[sctpeln()\+\-*\/^]|[0-9]+/g);
+        newExpression = newExpression.map(char => {
+            if (!isNaN(Number(char))) { return Number(char) }
+            else { return char }
+        })
+        console.log(newExpression);
+        console.log(calculate(newExpression));
     }
 }
 
@@ -87,6 +95,16 @@ document.getElementById("exp-input").addEventListener("keydown", event => {
 const calculate = expression => {
     let exp = expression.slice();
     while (exp.length > 1) {
+
+        while (exp.includes("p")) {
+            let index = exp.indexOf("p");
+            exp.splice(index, 1, Math.PI);
+        }
+
+        while (exp.includes("e")) {
+            let index = exp.indexOf("e");
+            exp.splice(index, 1, Math.E);
+        }
         
         while (exp.includes("(")) {
             let parenNum = 1;
@@ -100,10 +118,68 @@ const calculate = expression => {
             exp.splice(exp.indexOf("("), innerExp.length + 2, calculate(innerExp));
         }
 
+        let i = 0
+        while (i <= expression.length) {
+            if (exp[i] === "-" && (i === 0 || typeof exp[i - 1] !== "number")) { exp.splice(i, 1, -1, "*"); }
+            i++;
+        }
+
+        // let specials = ["s", "c", "t", "S", "T", "C"];
+
+        // for (char in specials) 
+
+        while (exp.includes("s")) {
+            let index = exp.indexOf("s");
+            let add = Math.sin(exp[index + 1]);
+            exp.splice(index, 2, add);            
+        }
+
+        while (exp.includes("c")) {
+            let index = exp.indexOf("c");
+            let add = Math.cos(exp[index + 1]);
+            exp.splice(index, 2, add);
+        }
+
+        while (exp.includes("t")) {
+            let index = exp.indexOf("t");
+            let add = Math.tan(exp[index + 1]);
+            exp.splice(index, 2, add);
+        }
+
+        while (exp.includes("S")) {
+            let index = exp.indexOf("S");
+            let add = Math.asin(exp[index + 1]);
+            exp.splice(index, 2, add);
+        }
+
+        while (exp.includes("C")) {
+            let index = exp.indexOf("C");
+            let add = Math.acos(exp[index + 1]);
+            exp.splice(index, 2, add);
+        }
+
+        while (exp.includes("T")) {
+            let index = exp.indexOf("T");
+            let add = Math.atan(exp[index + 1]);
+            exp.splice(index, 2, add);
+        }
+
+        while (exp.includes("l")) {
+            let index = exp.indexOf("l");
+            let add = Math.log10(exp[index + 1]);
+            exp.splice(index, 2, add);
+        }
+
+        while (exp.includes("n")) {
+            let index = exp.indexOf("n");
+            let add = Math.log(exp[index + 1]);
+            exp.splice(index, 2, add);
+        }
+
         while (exp.includes("^")) {
             let index = exp.indexOf("^");
             let add = exp[index - 1]**exp[index + 1];
-            exp.splice(index-1, 3, add);
+            exp.splice(index-1, 2, add);
         }
 
         while (exp.includes("*") || exp.includes("/")) {
@@ -146,7 +222,9 @@ const calcLog10 = exp => Math.log10(calculate(exp));
 
 const calcLn = exp => Math.log(calculate(exp));
 
-let exp2 = ["(", 1, "-", 3, "*", "(", 2, "/", 3, "-", 1, ")", "+", 2, ")", "*", 2, "-", "(", 2, "+",1, "*", 3, "*", "(" , 3, "+", 1, "-", 1, ")", ")", "^", 2]
+let exp = [2,"+",2]
+let exp2 = ["(", 1, "-", 3, "*", "l", "(", 2, "/", 3, "-", 1, ")", "+", 2, "+", "e", ")", "*", 2, "-", "(", 2, "+",1, "*", 3, "*", "(" , 3, "+", 1, "-", 1, ")", ")", "^", 2]
+let exp3 = ["-", 4, "+", "s", "(", 45, "*", "p", "*", "(", 2, ")", "*", "l", "(", 104, ")", ")", "+", "e"]
 // let exp2 = [1, "-", 3]
 
 // ***/-+++
